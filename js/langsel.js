cd3305ab9ab553d7c22752b2470e3b36c39e0129
Remi Remino <remi@remino.net>
2013-09-26 08:39:42 +0900

    Choose language selected by browser

---
+++ js/langsel.js
@@ -0,0 +1,28 @@
+(function() {
+  var $;
+
+  $ = jQuery;
+
+  $.langSel = function() {
+    return $.ajax({
+      dataType: 'jsonp',
+      url: "http://ajaxhttpheaders.appspot.com",
+      success: function(headers) {
+        var $link, locale, url;
+        locale = headers['Accept-Language'].substr(0, 2);
+        $link = $("[data-langsel-locale='" + locale + "']");
+        url = $link.attr('data-href') || $link.attr('href');
+        if (url) {
+          return self.location.replace(url);
+        }
+      }
+    });
+  };
+
+}).call(this);
+(function() {
+  jQuery(function() {
+    return $.langSel();
+  });
+
+}).call(this);
