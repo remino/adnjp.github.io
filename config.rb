895d5d7abb5ab7ba178104b3043591837a2cf177
Remi Remino <remi@remino.net>
2013-09-25 23:17:18 +0900

    Localise blog

--- config.rb
+++ config.rb
@@ -2,13 +2,27 @@ require 'slim/smart'
 
 Time.zone = 'Tokyo'
 
+# Ignore, I18n, and blogs are ordered below for a localised blog
+
+ignore %r{^blog/}
+
+activate :i18n, mount_at_root: false, path: '/:locale/'
+
 activate :blog do |blog|
-  blog.permalink = ':title.html'
-  blog.sources = 'articles/:year-:month-:day-:title.html'
+  blog.name = 'en'
+  blog.permalink = 'en/blog/:title.html'
+  blog.sources = 'en/blog/:year-:month-:day-:title.html'
+  blog.taglink = 'en/blog/:tag.html'
+end
+
+activate :blog do |blog|
+  blog.name = 'ja'
+  blog.permalink = 'ja/blog/:title.html'
+  blog.sources = 'ja/blog/:year-:month-:day-:title.html'
+  blog.taglink = 'ja/blog/:tag.html'
 end
 
 activate :directory_indexes
-activate :i18n, mount_at_root: :en, path: '/:locale/'
 activate :livereload
 
 set :css_dir, 'css'
