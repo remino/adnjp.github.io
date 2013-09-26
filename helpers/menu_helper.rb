91939abb0c982b96945a54fc1b2f22db52b51769
Remi Remino <remi@remino.net>
2013-09-26 12:59:49 +0900

    Add language toggle

--- helpers/menu_helper.rb
+++ helpers/menu_helper.rb
@@ -1,10 +1,22 @@
 module MenuHelper
   ::NEWLINE = "\n" unless defined? ::NEWLINE
 
+  def lang_menu(options = {})
+    options.reverse_merge! class: 'nav'
+
+    items = langs.each.map do |lang|
+      next unless lang != @lang
+      path = current_page.url.gsub %r{^/#{@lang}/}, "/#{lang}/"
+      content_tag :li, link_to(t("locale.#{lang}"), path)
+    end.join ''
+
+    menu_list items, options
+  end
+
   def menu(name, options = {})
     options.reverse_merge! class: 'nav'
     items = data.menu[name].keys.map { |item| menu_item name, item }.join ''
-    content_tag :ul, items, options
+    menu_list items, options
   end
 
   def menu_item(menu, item, item_options = {})
@@ -18,5 +30,10 @@ module MenuHelper
     link = link_to t("menu.#{menu}.#{item}"), href, options
 
     content_tag :li, link, item_options[:li]
+  end
+
+  def menu_list(content, options = {})
+    options.reverse_merge! tag: :ul
+    content_tag options.delete(:tag), content, options
   end
 end
